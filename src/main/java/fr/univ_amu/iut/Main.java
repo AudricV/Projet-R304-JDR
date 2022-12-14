45fdc6f0cf26a793b10fa165631ee3e88cea1538
AudricV <74829229+AudricV@users.noreply.github.com>
2022-12-14 13:43:10 +0100

    Add initial validation of arguments and show a help message
    This validation shows a message in the case of error in the arguments
    passed and exits with an error code 1.
    
    A help message is shown in the case no arguments, -h or --help is entered.
    An exit code 0 is returned in this case.
    
    String resources have been added in a separate utility class, Messages.

--- src/main/java/fr/univ_amu/iut/Main.java
+++ src/main/java/fr/univ_amu/iut/Main.java
@@ -1,7 +1,91 @@
 package fr.univ_amu.iut;
 
+import java.util.*;
+
+import static fr.univ_amu.iut.Messages.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Jeu de rôles - R304");
+    /**
+     * The number of required arguments to run the game.
+     */
+    private static final int ARGS_REQUIRED_COUNT = 6;
+
+    public static void main(final String[] args) {
+        System.out.println(GAME_TITLE);
+
+        // Show the help message in the case no arguments are passed or the first argument is -h or
+        // --help (case sensitive)
+        if (args == null || args.length == 0
+                || args[0].equalsIgnoreCase(HELP_SHORT_ARG_NAME)
+                || args[0].equalsIgnoreCase(HELP_LONG_ARG_NAME)) {
+            showHelp();
+        }
+
+        // Suppress the null pointer exception warning, as the program is stopped above if the args
+        // string array is null
+        //noinspection ConstantConditions
+        if (args.length < ARGS_REQUIRED_COUNT) {
+            System.out.println(INVALID_ARGUMENTS_COUNT_MESSAGE);
+            errorExit();
+        }
+
+        // Parse arguments from the string args array
+        final Map<String, String> mapArgs = new HashMap<>();
+        // We browse each required argument with its value
+        for (int i = 0; i < ARGS_REQUIRED_COUNT; i += 2) {
+            mapArgs.put(args[i], args[i + 1]);
+        }
+
+        checkRequiredArguments(mapArgs);
+
+        System.out.println(CSV_LOAD_TITLE);
+    }
+
+    /**
+     * Show the help message of the game and stops its execution with an exit code 0.
+     */
+    private static void showHelp() {
+        System.out.println(HELP_CONTENT);
+        System.exit(0);
+    }
+
+    /**
+     * Stops the game's execution with an exit code 1.
+     */
+    private static void errorExit() {
+        System.exit(1);
+    }
+
+    /**
+     * Show the missing argument message of a given file type and exit with an error code using
+     * {@link #errorExit()}.
+     *
+     * @param argumentType a "human" file type argument, to be inserted as {@code ARGUMENT_TYPE} in
+     *                     the following message:
+     *                     {@code "Usage invalide : argument " + ARGUMENT_TYPE + " manquant"}
+     */
+    private static void showMissingFileArgValueMessageAndExit(final String argumentType) {
+        System.out.printf(Messages.MISSING_ARGUMENTS_MESSAGE, argumentType);
+        errorExit();
+    }
+
+    /**
+     * Check whether required arguments have been provided.
+     *
+     * @param mapArgs the {@link Map} of arguments and values, which comes from the string array of
+     *                arguments; keys must be arguments and values the value of arguments
+     */
+    private static void checkRequiredArguments(final Map<String, String> mapArgs) {
+        if (!mapArgs.containsKey(CSV_CHARACTER_ARG_NAME)) {
+            showMissingFileArgValueMessageAndExit("personnages");
+        }
+
+        if (!mapArgs.containsKey(CSV_EVENTS_ARG_NAME)) {
+            showMissingFileArgValueMessageAndExit("évènements");
+        }
+
+        if (!mapArgs.containsKey(CSV_ITEMS_ARG_NAME)) {
+            showMissingFileArgValueMessageAndExit("items");
+        }
     }
 }
---
+++ src/main/java/fr/univ_amu/iut/Messages.java
@@ -0,0 +1,38 @@
+package fr.univ_amu.iut;
+
+/**
+ * String resources which are used for system outputs.
+ *
+ * <p>
+ * Some of them can be string formats.
+ * </p>
+ */
+public final class Messages {
+    // Command-line arguments names
+    static final String CSV_CHARACTER_ARG_NAME = "--characters-file";
+    static final String CSV_EVENTS_ARG_NAME = "--events-file";
+    static final String CSV_ITEMS_ARG_NAME = "--items-file";
+    static final String HELP_SHORT_ARG_NAME = "-h";
+    static final String HELP_LONG_ARG_NAME = "-help";
+    // Game section titles
+    static final String GAME_TITLE = "Jeu de rôles - R304";
+    static final String CSV_LOAD_TITLE = "Chargement des fichiers ...";
+    static final String HELP_CONTENT = "Usage :\n"
+            + HELP_SHORT_ARG_NAME
+            + ", "
+            + HELP_LONG_ARG_NAME
+            + " en premier argument ou aucun argument : afficher cette aide\n"
+            + CSV_CHARACTER_ARG_NAME
+            + " : emplacement vers le fichier CSV des personnages\n"
+            + CSV_EVENTS_ARG_NAME
+            + " : emplacement vers le fichier CSV des évènements\n"
+            + CSV_ITEMS_ARG_NAME
+            + " : emplacement vers le fichier CSV des items\n";
+    // Error messages
+    static final String INVALID_ARGUMENTS_COUNT_MESSAGE =
+            "Nombre d'arguments invalide : 3 paramètres avec leur valeur sont attendus";
+    static final String MISSING_ARGUMENTS_MESSAGE = "Usage invalide : argument %s manquant";
+
+    private Messages() {
+    }
+}
