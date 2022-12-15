f4bfb83036dd55e474b10212032711bcc05e9632
AudricV <74829229+AudricV@users.noreply.github.com>
2022-12-16 00:30:53 +0100

    Translate remaining strings in English

--- src/main/java/fr/univ_amu/iut/Messages.java
+++ src/main/java/fr/univ_amu/iut/Messages.java
@@ -16,37 +16,37 @@ final class Messages {
     static final String HELP_LONG_ARG_NAME = "-help";
 
     // Game section titles
-    static final String GAME_TITLE = "Jeu de rôles - R304";
-    static final String CSV_LOAD_TITLE = "Chargement des fichiers ...";
-    static final String CSV_LOAD_TITLE_COMPLETE = "Chargement des fichiers terminé";
+    static final String GAME_TITLE = "Role play - R304";
+    static final String CSV_LOAD_TITLE = "Loading files...";
+    static final String CSV_LOAD_TITLE_COMPLETE = "Files loaded.";
     static final String HELP_CONTENT = "Usage :\n"
             + HELP_SHORT_ARG_NAME
             + ", "
             + HELP_LONG_ARG_NAME
-            + " en premier argument ou aucun argument : afficher cette aide\n"
+            + " as a first argument or no argument: show this help\n"
             + CSV_CHARACTER_ARG_NAME
-            + " : emplacement vers le fichier CSV des personnages\n"
+            + ": path to the characters' CSV file\n"
             + CSV_EVENTS_ARG_NAME
-            + " : emplacement vers le fichier CSV des évènements\n"
+            + ": path to the characters' CSV events\n"
             + CSV_ITEMS_ARG_NAME
-            + " : emplacement vers le fichier CSV des items\n";
+            + ": path to the characters' CSV items\n";
 
     // Error messages
     static final String INVALID_ARGUMENTS_COUNT_MESSAGE =
-            "Nombre d'arguments invalide : 3 paramètres avec leur valeur sont attendus";
-    static final String MISSING_ARGUMENTS_MESSAGE = "Usage invalide : argument %s manquant";
+            "Invalid arguments count: 3 parameters with their value are required";
+    static final String MISSING_ARGUMENTS_MESSAGE = "Invalid usage : argument %s is missing";
     static final String INVALID_FILE =
-            "L'emplacement du fichier des %s est inexistant, inaccessible ou fait référence à un dossier.";
+            "%s' path references a nonexistent or an inaccessible file, or a folder.";
     static final String PARSING_ERROR =
-            "Erreur lors de l'analyse du fichiers des %s";
+            "Error when parsing %s' file";
 
     // Elements
-    static final String CHARACTERS = "personnages";
-    static final String EVENTS = "évènements";
+    static final String CHARACTERS = "characters";
+    static final String EVENTS = "events";
     static final String ITEMS = "items";
 
     // Game begin
-    static final String WELCOME_MESSAGE = "Welcome to Random JDR Quest !";
+    static final String WELCOME_MESSAGE = "Welcome to Random JDR Quest!";
     static final String SELECT_CHARACTER_CLASS = "Please select you character's class using the numbers provided:";
 
     static final String SELECT_CHARACTER_NAME = "Choose your character's name:";
@@ -59,16 +59,16 @@ final class Messages {
     static final String PLAYER_ATTACKS_ENEMY = "%s attacks the %s!\n";
     static final String ENEMY_ATTACKS_YOU = "%s attacks you!";
     static final String CHARACTER_DEALS_DAMAGE = "It deals %d damage(s)!\n";
-    static final String PLAYER_HP_RECOVERED = "You recovered %d HP !";
+    static final String PLAYER_HP_RECOVERED = "You recovered %d HP!";
     static final String PLAYER_WIN = "You win!";
-    static final String PLAYER_WIN_GOLD = "You win %d gold !";
+    static final String PLAYER_WIN_GOLD = "You win %d gold!";
     static final String PLAYER_WIN_XP = "You win %d XP!";
 
     // Levels
     static final String NEXT_LEVEL = "You can go to the next level...";
 
     // Game end
-    static final String NO_HP_LEFT = "No HP left, You are dead...";
+    static final String NO_HP_LEFT = "No HP left, you are dead...";
     static final String GAME_OVER = "Game Over!";
 
     private Messages() {
